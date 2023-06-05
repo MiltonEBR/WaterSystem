07e3e0218947204fca353cf6f174cdf04d5c4b55
Milton <a01634505@tec.mx>
2023-06-05 12:44:19 -0600

    Merge with frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import router from './routes/router';
 import cors from 'cors'
+import path from 'path'
 
+const _app_folder = path.join(__dirname, '..', '..', 'frontend', 'dist');
 const port = Number(process.env.PORT) || 4000;
 const app = express()
+
+
+app.use('/', express.static(_app_folder));
+app.use('*', express.static(_app_folder));
 
 app.use(cors())
 app.use(bodyParser.json())
